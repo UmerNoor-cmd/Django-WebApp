b83160a21c2a46bb0d45e8b75cfc3484fa6f8d9a
UmerNoor-cmd <umernoor6660@gmail.com>
2024-05-18 19:27:59 +0500

    cleaned up code

--- courses/urls.py
+++ courses/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
-from .views import login_or_register,course_search,register_courses,add_course,course_detail
+from .views import login_or_register,course_search,add_course,course_detail
 
 urlpatterns = [
     path('', views.login_or_register, name='login_or_register'),  # Add this line
     path('course_search/', views.course_search, name='course_search'),
-    path('register_courses/', register_courses, name='register_courses'),
     path('generate_reports/', views.generate_reports, name='generate_reports'),  # Add this line
     path('course_detail/', views.course_detail, name='course_detail'),
     path('add_course/', add_course, name='add_course'),
-    path('view_schedule/', views.view_schedule, name='view_schedule'),
     
 ]
--- courses/views.py
+++ courses/views.py
@@ -219,56 +219,7 @@ def generate_reports(request):
 
 
 
-def register_courses(request):
-    if request.method == 'POST':
-        selected_course_ids = request.POST.getlist('courses')  # Get the list of selected course IDs from the form data
-        student_id = request.user.id  # Assuming the logged-in user is a student and their ID is stored in the session
-
-        # Check if any courses were selected
-        if not selected_course_ids:
-            messages.error(request, 'No courses selected.')
-            return redirect('course_search')
-
-        # Iterate through the selected course IDs and register the student for each course
-        for course_id in selected_course_ids:
-            course = Course.objects.get(pk=course_id)
-            # Check if the student is already registered for the course
-            if StudentReg.objects.filter(student_id=student_id, course_id=course_id).exists():
-                messages.warning(request, f'You are already registered for the course "{course.name}".')
-            else:
-                # Register the student for the course
-                StudentReg.objects.create(student_id=student_id, course_id=course_id)
-                messages.success(request, f'You have successfully registered for the course "{course.name}".')
-
-        return redirect('course_search')
-
-    # Redirect to the course search page if the request method is not POST
-    return redirect('course_search')
 
 
-# def add_course(request):
-#     if request.method == 'POST':
-#         course_id = request.POST.get('course_id')
-#         # Retrieve the student and course objects
-#         student = Student.objects.get(id=request.user.id)  # Assuming you have a user ID in the request
-#         course = Course.objects.get(id=course_id)
 
-#         # Check if the student is already registered for the course
-#         if student.courses_registered.filter(id=course_id).exists():
-#             return render(request, 'add_course.html', {'error': 'You are already registered for this course.'})
 
-#         # Check if the course has available spots
-#         if course.capacity <= student.courses_registered.count():
-#             return render(request, 'add_course.html', {'error': 'This course is already at full capacity.'})
-
-#         # Add the course to the student's schedule
-#         student.courses_registered.add(course)
-#         return redirect('course_search')  # Redirect to course search page after successful addition
-
-#     else:
-#         return render(request, 'add_course.html')
-
-def view_schedule(request):
-    student = request.user
-    registrations = StudentReg.objects.filter(student=student)
-    return render(request, 'view_schedule.html', {'registrations': registrations})
