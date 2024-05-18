6d06e3d5461af282945146045dcf4ab0b157f226
UmerNoor-cmd <umernoor6660@gmail.com>
2024-05-18 16:40:15 +0500

    venv

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import login_or_register,course_search,register_courses,add_course
+from .views import login_or_register,course_search,register_courses,add_course,course_detail
 
 urlpatterns = [
     path('', views.login_or_register, name='login_or_register'),  # Add this line
     path('course_search/', views.course_search, name='course_search'),
-    path('course_detail/<int:course_id>/', views.course_detail, name='course_detail'),
+  path('course_detail/<int:course_id>/', views.course_detail, name='course_detail'),
     path('register_courses/', register_courses, name='register_courses'),
 
     path('add_course/', add_course, name='add_course'),
